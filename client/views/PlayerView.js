338c400b3cd2c2dfe09c3ce7a89d2723014e548f
ejk91 <enochkim91@gmail.com>
2017-02-21 11:33:32 -0800

    Pass tests for PlayerView

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -4,13 +4,18 @@ var PlayerView = Backbone.View.extend({
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
-
+  // we are listening to the player for events - ended event
   initialize: function() {
+    var context = this;
+    this.$el.on('ended', function() {
+      context.model.ended();
+    });
   },
 
   setSong: function(song) {
     this.model = song;
     this.render();
+
   },
 
   render: function() {
