4a3c32b9f6a8dcd2d259df7048c3741526f383ae
Enoch Kim <enochkim91@gmail.com>
2017-07-17 18:49:46 -0700

    Update app model to set current song to null on stop event

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -16,9 +16,15 @@ var AppModel = Backbone.Model.extend({
     // appmodel.currentSong = song;
       this.set('currentSong', song);
     }, this);
+
     params.library.on('enqueue', function(song) {
       this.get('songQueue').add(song);
     }, this);
+
+    this.get('songQueue').on('stop', function () {
+      this.set('currentSong', null);
+    }, this)
+
   }
 
 });
